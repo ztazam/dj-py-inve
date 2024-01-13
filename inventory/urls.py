d1271ad9818b11765e27ed0562740ec8297a5242
ztazam <jazelarayan@gmail.com>
2024-01-13 09:41:46 +0100

    first commit

---
+++ inventory/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', Index.as_view(), name='index'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('add_item/', AddItem.as_view(), name='add-item'),
+    path('edit-item/<int:pk>', EditItem.as_view(), name='edit-item'),
+    path('delete-item/<int:pk>', DeleteItem.as_view(),name='delete-item'),
+    path('signupi/', SignUpView.as_view(), name='signupi'),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+]
